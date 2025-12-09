aeebd937f4f0b38ebe2dff6ce2f3d5d6db34b7d1
luyao <luyaocore@163.com>
2025-12-09 15:18:41 +0800

    修改flutter项目默认后端地址

--- netbar_ops_flutter/lib/core/config/app_config.dart
+++ netbar_ops_flutter/lib/core/config/app_config.dart
@@ -3,7 +3,7 @@ class AppConfig {
   // API 基础URL，可通过 --dart-define BASE_URL=... 覆盖
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'http://10.0.0.58:8080/api/v1',
+    defaultValue: 'http://127.0.0.1:8080/api/v1',
   );
 
   // 应用名称
@@ -16,4 +16,3 @@ class AppConfig {
   static const int connectTimeout = 10000;
   static const int receiveTimeout = 10000;
 }
-
